1d19acfeaf04b5ca2271e830d91a1cccbaad383c
Kormos Áron <104300544+Kormos03@users.noreply.github.com>
2023-10-01 20:56:25 +0200

    Survey works, and now I test the textarea color.

--- fagyi_project/main.js
+++ fagyi_project/main.js
@@ -1,28 +1,37 @@
-function submit(e)
+
+function buttonClick(e)
 {
   let mail = document.getElementById('email').value;
   let textarea = document.getElementById('textarea').value;
   if(!mail.includes('@'))
-  {
-    console.log('aaaaa');
-   e.preventDefault();   
+  { 
+    e.preventDefault();
+    document.getElementById('error').textContent = 'Az email címnek tartalmaznia kell @ karaktert!';
     throw new Error('It has to contains @ character!');
   }
   else if (textarea.length < 10)
   {
+    e.preventDefault(); 
+    document.getElementById('error').textContent = 'Legalább 10 karakter szükséges a szöveges mezőbe az elküldéshez!';
     throw new Error('The minimum lenght of this text: 10 character');
   }
-  else
+  else if (textarea.length >= 10 && mail.includes('@'))
   {
+    document.getElementById('error').textContent = 'Legalá  bb 10 karakter szükséges az elküldéshez!';
     console.log('siker :)');
+    alert('Köszönjük az értékelést!')
   }
+}
+
+function frissit()
+{
+  console.log('Frissíts');
 }
 
 function init()
 {
-  document.getElementById('submitbtn').addEventListener('sub', submit);
-  document.getElementById('email').addEventListener('sub', submit);
-  document.getElementById('textarea').addEventListener('sub', submit);
+  document.getElementById('submit').addEventListener('click', buttonClick);
+  document.getElementById('form').addEventListener('frissit', frissit)
 }
 
 document.addEventListener('DOMContentLoaded', init);
